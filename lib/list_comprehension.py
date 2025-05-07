c037bc82cb6d4d603c7a5b86e4b369f8934f5240
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-05-07 14:21:47 +0300

    completed all tests as instructed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # Corrected the logic to return even numbers instead of boolean values
+    evens_list = [num for num in num_list if num % 2 == 0]
+    return evens_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [sentence + "!" for sentence in sentence_list]
+    return exclamation_list
